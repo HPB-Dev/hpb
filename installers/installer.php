5b1c2880aad2a0b8a453128102a3595d54d645fc
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2021-09-06 14:43:39 +0200

    Create installer.php

---
+++ installers/installer.php
@@ -0,0 +1,21 @@
+<?php
+echo "===================================";
+echo ">>>>>>>>         HPB       <<<<<<<<";
+echo "===================================";
+echo "";
+echo "Benvenuto nel programma di installazione di HPB!";
+$s = readline("Vuoi procedere con l'installazione? [Y / n]");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
