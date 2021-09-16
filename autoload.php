6cae9c384ba7b1f16a450581789de75b4fd4b2e8
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2021-09-16 20:15:58 +0200

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Autoload ufficiale di HPB
+*/
+
+require("hpb/MainClass.php");
+require("hpb/MySQL.php");
+require("hpb/GetDepend.php");
+require("hpb/HTMLUtils.php");
+require("hpb/LetsCrypt.php");
